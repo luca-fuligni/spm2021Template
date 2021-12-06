79957763ef9976df49d9a5d0a490b84238cbfaa0
Fabrizio <fabrizio.fornari@unicam.it>
2021-12-06 17:20:18 +0100

    changed selenium test for githubaction

--- src/test/java/pros/unicam/spm2021/practice/AcceptanceTest/MyWebappSeleniumAcceptanceTest.java
+++ src/test/java/pros/unicam/spm2021/practice/AcceptanceTest/MyWebappSeleniumAcceptanceTest.java
@@ -102,7 +102,7 @@ class MyWebappSeleniumAcceptanceTest {
 	//@Disabled
 	@Tag("AcceptanceTest")
 	void testMyGitHubAction() {
-		  driver.navigate().to(baseUrl);
+		  driver.get().to(baseUrl);
 	      System.out.println("Title is: "+driver.getTitle());
 	      assertTrue(driver.getTitle().contains("LambdaTest"));
 	}
